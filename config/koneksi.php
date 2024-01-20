c3604762015156f0b724c0408f141ac8c3a1a7de
Diki Hamdani <36911342+Dikihmd01@users.noreply.github.com>
2024-01-20 20:14:11 +0700

    update configuration

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host = "reltaldiki-db.mysql.database.azure.com";
-$user = "diki1234";
-$pass = "Tanggal29";
+$host = "localhost";
+$user = "root";
+$pass = "";
 $database = "rental_mobil_dikihamdani";
 $koneksi = new mysqli($host, $user, $pass, $database);
 if (mysqli_connect_errno()) {
